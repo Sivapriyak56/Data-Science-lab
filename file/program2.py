12b8115f0c82c2de56dbbd397da31781b6b808c2
Sivapriyak56 <79069851+Sivapriyak56@users.noreply.github.com>
2022-01-22 00:19:57 -0500

    Delete file directory

--- file/program2.py
+++
@@ -1,8 +0,0 @@
-file = open('text.txt','r')
-count =0
-content=file.read()
-colist = content.split("\n")
-for i in colist:
-    if i:
-        count = count+1
-print("No.of line :" ,count)
--- file/program3.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-
-def wd_c(fname):
-    with open(fname) as f:
-        return Counter(f.read().split())
-print("number: ",wd_c("text.txt"))
